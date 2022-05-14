86c818afced8f198e748e969efa05034368b68b3
Ebarretorho <ebarretorho@gmail.com>
2022-05-14 12:23:39 -0400

    last changes saved

--- app.py
+++ app.py
@@ -107,39 +107,3 @@ def data_3_func():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app = Flask(__name__)
-
-hello_dict = {"Hello": "World!"}
-
-
-@app.route("/")
-def home():
-    return "Hi"
-
-
-@app.route("/normal")
-def normal():
-    return hello_dict
-
-
-@app.route("/jsonified")
-def jsonified():
-    return jsonify(hello_dict)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
